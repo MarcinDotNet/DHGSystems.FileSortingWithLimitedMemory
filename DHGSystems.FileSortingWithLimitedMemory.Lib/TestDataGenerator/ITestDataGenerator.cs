053ac244d58a6892177ea072e65018dc60935a89
Marcin Pazgier <mpazgier@yahoo.co.uk>
2024-09-14 23:26:07 +0200

    added testfiles

---
+++ DHGSystems.FileSortingWithLimitedMemory.Lib/TestDataGenerator/ITestDataGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DHGSystems.FileSortingWithLimitedMemory.Lib.TestDataGenerator
+{
+    public interface ITestDataGenerator
+    {
+        void GenerateTestFile(long numberOfRows, int averageStringSize, string outputFileFullFileName);
+    }
+}
