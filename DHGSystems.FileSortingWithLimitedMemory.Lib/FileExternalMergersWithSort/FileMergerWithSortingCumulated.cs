54af8003498e3da1a31844600941d34c479685f3
Marcin Pazgier <mpazgier@yahoo.co.uk>
2024-09-17 09:06:58 +0200

    message added

---
+++ DHGSystems.FileSortingWithLimitedMemory.Lib/FileExternalMergersWithSort/FileMergerWithSortingCumulated.cs
@@ -0,0 +1,87 @@
+﻿using DHGSystems.FileSortingWithLimitedMemory.Lib.Model;
+using System.Linq;
+
+namespace DHGSystems.FileSortingWithLimitedMemory.Lib.FileExternalMergersWithSort
+{
+    public class FileMergerWithSortingCumulated : IFileMergerWithSorting
+    {
+        public void MergeFilesWithSort(string[] filesToMerge, string outputFilePath)
+        {
+            int startFileCount = filesToMerge.Length;
+            
+            // open files 
+            StreamReader[] streamReaders = new StreamReader[startFileCount];
+            for (int i = 0; i < filesToMerge.Length; i++)
+            {
+               streamReaders[i] = new StreamReader(filesToMerge[i]);
+            }
+
+            for (int i = 0; i < startFileCount; i++)
+            {
+                
+            }
+            
+            List<ProcessingStreamToMerge> list = new List<ProcessingStreamToMerge>();
+            for (int i = 0; i < filesToMerge.Length; i++)
+            {
+                list.Add(new ProcessingStreamToMerge(i, filesToMerge[i]));
+            }
+
+            bool elementRead;
+
+            //Load first element from each file
+            for (int i = list.Count - 1; i > -1; i--)
+            {
+                elementRead = list[i].LoadNextEntry();
+                if (!elementRead)
+                {
+                    list.RemoveAt(i);
+                }
+            }
+
+            using (StreamWriter outputFile = new StreamWriter(outputFilePath))
+            {
+                outputFile.AutoFlush = false;
+                ProcessingStreamToMerge item;
+                bool firstLine = true;
+                int flushCount = 0;
+                while (list.Any())
+                {
+                    // to not set new line at the beginning of the file and to not set new line at the end of the file
+                    if (!firstLine)
+                    {
+                        outputFile.WriteLine();
+                    }
+                    else
+                    {
+                        firstLine = false;
+                    }
+
+                    item = list.OrderBy(x => x.LastEntry.Name).ThenBy(x => x.LastEntry.Number).First();
+                  
+                    outputFile.Write(item.LastEntry.Number);
+                    outputFile.Write(".");
+                    outputFile.Write(item.LastEntry.Name);
+
+                    elementRead = item.LoadNextEntry();
+                    if (!elementRead)
+                    {
+                        list.RemoveAll(x => x.Id == item.Id);
+                    }
+                    flushCount++;
+                    if (flushCount == 5000)
+                    {
+                        outputFile.Flush();
+                        flushCount = 0;
+                    }
+                }
+                outputFile.Flush();
+            }
+
+            foreach(var file in filesToMerge)
+            {
+                File.Delete(file);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # DHGSystems.FileSortingWithLimitedMemory
+
+This app is developed to meet the this criteria:
+   - be able to process  1GB - 100 GB file with limited memory <16 GB
+   - solution will be run on multicore processor like Intel i9 9900K and fast SSD disks
+   - one file is processed at time
+
